53fa7f4c2124062e8dc1cdffbbf940677a03c50c
Ira Oprishko <ira.oprishko@gmail.com>
2015-12-14 18:15:50 +0200

    Initial interface version.

--- Spreadsheet/MainWindow.xaml.cs
+++ Spreadsheet/MainWindow.xaml.cs
@@ -22,11 +22,13 @@ namespace Spreadsheet
     /// 
     public class Item
     {
-        public string expression;
+        List<Item> dependencies;
+        bool expression;
+        string value;
         public string Value
         {
-            get { return expression; }
-            set { expression = value; }
+            get { return value; }
+            set { this.value = value; }
         }
     }
     public class ItemsRow
@@ -35,33 +37,73 @@ namespace Spreadsheet
     }
     public partial class MainWindow : Window
     {
+        List<List<Item>> data = new List<List<Item>> { new List<Item> { new Item() } };
         DataTable Excel = new DataTable();
         char column = 'A';
-        int row = 1;
-
+        public List<List<Item>> Data { get { return data; } }
+        public void addRow()
+        {
+            data.Add(new List<Item>(data[0].Count - 1));
+            for (int i = 0; i < data[0].Count; i++)
+            {
+                data[data.Count - 1].Add(new Item());
+            }
+        }
+        public void addColumn()
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i].Add(new Item());
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            addColumn();
+            Excel.Columns.Add((column++).ToString());
+            Excel.Columns.Add((column++).ToString());
+            addRow();
+            Excel.Rows.Add(Excel.NewRow());
+            Excel.Rows.Add(Excel.NewRow());
             dataGrid.ItemsSource = Excel.AsDataView();
         }
         private void OnButton1Click(object sender, RoutedEventArgs e)
         {
-            //Excel.Rows.Add(row.ToString());
             DataRow addedRow = Excel.NewRow();
             Excel.Rows.Add(addedRow);
-            ++row;
+            addRow();
         }
         private void OnButton2Click(object sender, RoutedEventArgs e)
         {
-
-            Excel.Columns.Add(column.ToString());
+            Excel.Columns.Add((column++).ToString());
             dataGrid.ItemsSource = Excel.AsDataView();
-            ++column;
+            addColumn();
+        }
+        private void dataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex().ToString();
         }
         private void Calc(object sender, RoutedEventArgs e)
         {
             Tree<ArithmExpr> t = new Tree<ArithmExpr>();
             //textBox1.Text = t.calculate(new ArithmExpr(textBox.Text)).ToString();
+        }
+
+        private void dataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            data[e.Row.GetIndex()][e.Column.Header.ToString()[0] - 'A'].Value = ((TextBox)e.EditingElement).Text;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            string debug = "";
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = 0; j < data[0].Count; j++)
+                    debug += data[i][j].Value + "|";
+                debug += "\n";
+            }
+            MessageBox.Show(debug);
         }
     }
 }
